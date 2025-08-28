e03d825833e33f08792e1ac7356a08fc660b4d52
wiral <wiralodrasaputra07@gmail.com>
2025-08-28 12:33:51 +0700

    Add WeddingPackagesTable configuration for wedding packages listing

--- app/Filament/Resources/WeddingPackages/Tables/WeddingPackagesTable.php
+++ app/Filament/Resources/WeddingPackages/Tables/WeddingPackagesTable.php
@@ -7,8 +7,12 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Schemas\Components\View;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 
 class WeddingPackagesTable
 {
@@ -16,12 +20,33 @@ class WeddingPackagesTable
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('weddingOrganizer.name'),
+
+                Tables\Columns\ImageColumn::make('thumbnail'),
+
+                Tables\Columns\IconColumn::make('is_popular')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Popular'),
             ])
             ->filters([
                 TrashedFilter::make(),
+                SelectFilter::make('city_id')
+                    ->label('City')
+                    ->relationship('city', 'name'),
+
+                SelectFilter::make('wedding_organizer_id')
+                    ->label('Wedding Organizer')
+                    ->relationship('weddingOrganizer', 'name'),
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
             ])
             ->toolbarActions([
