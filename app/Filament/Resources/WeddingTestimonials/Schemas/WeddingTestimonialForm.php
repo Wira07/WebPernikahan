5d3394332f65cf6679ebad438eb6cdbc23db8336
wiral <wiralodrasaputra07@gmail.com>
2025-08-30 14:03:34 +0700

    Update WeddingTestimonialForm

--- app/Filament/Resources/WeddingTestimonials/Schemas/WeddingTestimonialForm.php
+++ app/Filament/Resources/WeddingTestimonials/Schemas/WeddingTestimonialForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\WeddingTestimonials\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms;
 
 class WeddingTestimonialForm
 {
@@ -10,7 +11,26 @@ class WeddingTestimonialForm
     {
         return $schema
             ->components([
-                //
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('occupation')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\FileUpload::make('photo')
+                    ->image()
+                    ->required(),
+
+                Forms\Components\Select::make('wedding_package_id')
+                    ->relationship('weddingpackage', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                Forms\Components\Textarea::make('message')
+                    ->required(),
             ]);
     }
 }
