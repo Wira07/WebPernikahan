18aa41b87a35fe822a474520270142fffefebb65
wiral <wiralodrasaputra07@gmail.com>
2025-08-30 14:03:34 +0700

    Update BookingTransactionsTable

--- app/Filament/Resources/BookingTransactions/Tables/BookingTransactionsTable.php
+++ app/Filament/Resources/BookingTransactions/Tables/BookingTransactionsTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\BookingTransactions\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class BookingTransactionsTable
@@ -13,10 +15,26 @@ class BookingTransactionsTable
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\ImageColumn::make('weddingPackage.thumbnail'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('booking_trx_id')
+                    ->searchable(),
+
+                Tables\Columns\IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Terverifikasi'),
             ])
             ->filters([
-                //
+                SelectFilter::make('wedding_package_id')
+                    ->label('Wedding Package')
+                    ->relationship('weddingPackage', 'name'),
             ])
             ->recordActions([
                 EditAction::make(),
